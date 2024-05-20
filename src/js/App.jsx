20d7a48d32a0d495ce39a0463c1a5ddc1acf1e14
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-05-20 16:05:05 +0900

    Add basic UI

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 const App = () => {
   return (
-    <Typography variant="h1" gutterBottom>
-      Hi, this is a React app!
-    </Typography>
+    <Box 
+      sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: 2,
+      m: 2
+      }}
+    >
+      <Typography variant="h2" gutterBottom>
+      Speech to text
+      </Typography>
+
+      <Button variant="contained" color="primary">
+      Start Recording
+      </Button>
+
+      <Button variant="contained" color="secondary">
+      Stop Recording
+      </Button>
+
+      <TextareaAutosize
+      minRows={10}
+      placeholder="Generated text will appear here..."
+      style={{ width: '100%', padding: '1em' }}
+      />
+    </Box>
   );
 }
 
