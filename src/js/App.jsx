73c8fb5a8d0e0c406298c6fdb98a25f26c3b8b19
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-05-20 17:39:41 +0900

    Add mvp speech to text
    Have to degrade capacitor to 5

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { SpeechRecognition } from "@capacitor-community/speech-recognition";
 
 const App = () => {
+  const [transcript, setTranscript] = useState('');
+
+  const startRecording = async () => {
+    const { speechRecognition } = await SpeechRecognition.requestPermissions();
+    if (speechRecognition === 'granted') {
+      SpeechRecognition.start({
+        language: 'en-US',
+        maxResults: 2,
+        prompt: 'Say something',
+        partialResults: true,
+        popup: true,
+      });
+      SpeechRecognition.addListener('partialResults', (data) => {
+        setTranscript(data.matches[0]);
+      });
+    }
+  };
+  
+  const stopRecording = async () => {
+    SpeechRecognition.removeAllListeners();
+    await SpeechRecognition.stop();
+  };
+
   return (
     <Box 
       sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      gap: 2,
-      m: 2
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 2,
+        m: 2
       }}
     >
       <Typography variant="h2" gutterBottom>
-      Speech to text
+        Speech to text
       </Typography>
 
-      <Button variant="contained" color="primary">
-      Start Recording
+      <Button variant="contained" color="primary" onClick={startRecording}>
+        Start Recording
       </Button>
 
-      <Button variant="contained" color="secondary">
-      Stop Recording
+      <Button variant="contained" color="secondary" onClick={stopRecording}>
+        Stop Recording
       </Button>
 
       <TextareaAutosize
-      minRows={10}
-      placeholder="Generated text will appear here..."
-      style={{ width: '100%', padding: '1em' }}
+        minRows={10}
+        placeholder="Generated text will appear here..."
+        style={{ width: '100%', padding: '1em' }}
+        value={transcript}
       />
     </Box>
   );
