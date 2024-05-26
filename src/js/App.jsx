1e8c55a56f3819bd3ad29fe7ad0d50ed1ea021b7
AlvaroAsial <alvaro@asial.co.jp>
2024-05-27 00:19:31 +0900

    Pushing branch

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Fab from '@mui/material/Fab';
+import Button from '@mui/material/Button';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import MicIcon from '@mui/icons-material/Mic';
+import StopIcon from '@mui/icons-material/Stop';
+import { SpeechRecognition } from "@capacitor-community/speech-recognition";
+import SalesReportEx from './SalesReportEx';
 
 const App = () => {
+  const [transcript, setTranscript] = useState('');
+  const [isRecording, setIsRecording] = useState(false);
+  const [showSalesReport, setShowSalesReport] = useState(false);
+
+  const startRecording = async () => {
+    if (Capacitor.platform == 'web') {
+      alert('Speech recognition is not available here');
+      return;
+    }
+
+    const { speechRecognition } = await SpeechRecognition.requestPermissions();
+    if (speechRecognition === 'granted') {
+      SpeechRecognition.start({
+        language: 'en-US',
+        maxResults: 2,
+        prompt: 'Say something',
+        partialResults: true,
+        popup: true,
+      });
+      SpeechRecognition.addListener('partialResults', (data) => {
+        setTranscript(data.matches[0]);
+      });
+      setIsRecording(true);
+    }
+  };
+  
+  const stopRecording = async () => {
+    if (Capacitor.platform == 'web') {
+      alert('Speech recognition is not available here');
+      return;
+    }
+
+    SpeechRecognition.removeAllListeners();
+    await SpeechRecognition.stop();
+    setIsRecording(false);
+  };
+
   return (
-    <Typography variant="h1" gutterBottom>
-      Hi, this is a React app!
-    </Typography>
+    <Box 
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Box sx={{ p:4 }}>
+        <Typography variant="h3" gutterBottom>
+          Speech to text
+        </Typography>
+      </Box>
+      <Box>
+      {showSalesReport && <SalesReportEx />}
+      </Box>
+  
+      <Box sx={{ width:'80%', flex: 1 }}>
+        <TextareaAutosize
+          minRows={10}
+          placeholder="Generated text will appear here..."
+          style={{ width: '100%', border: 'none'}}
+          value={transcript}
+        />
+      </Box>
+
+  
+      <Box sx={{ p:8, position: 'fixed', bottom: 0, width: '100%', display: 'flex', justifyContent: 'center' }}>
+        {!isRecording ? (
+          <Fab color="primary" onClick={startRecording}>
+            <MicIcon />
+          </Fab>
+        ) : (
+          <Fab color="secondary" onClick={stopRecording}>
+            <StopIcon />
+          </Fab>
+        )}
+        <Button variant="contained" onClick={() => setShowSalesReport(!showSalesReport)}>
+          Toggle Sales Report
+        </Button>
+      </Box>
+      
+    </Box>
   );
 }
 
