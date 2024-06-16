f265d93702c4b27d88fa7ec2aa7d5c4a3a78b397
AlvaroAsial <alvaro@asial.co.jp>
2024-06-17 08:10:21 +0900

    Adding back button

--- src/js/gemini.js
+++ src/js/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyDJ0HYT21EBFv1OwnLM0eKh-dAUE6qb88s"
+const API_KEY = ""
 const genAI = new GoogleGenerativeAI(API_KEY);
 const geminiModel = genAI.getGenerativeModel({ model: "gemini-pro"});
 
--- src/js/screens/ReportPreviewDownload.jsx
+++ src/js/screens/ReportPreviewDownload.jsx
@@ -5,15 +5,26 @@ import { Typography } from '@mui/material';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
 import Fab from '@mui/material/Fab';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const ReportPreviewDownload = () => {
   const location = useLocation();
   const htmlContent = location.state.htmlContent;
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate('/', {}); 
+  };
 
   return (
     <Box display="flex" flexDirection="column" style={{ margin: '10px'}} gap={2} mx={2}>
-    <Typography variant="h3" gutterBottom> Sales Report Preview</Typography>
+      <Box>
+        <Fab style={{ position: 'relative', marginBottom: '10px' }} onClick={handleBack}>
+          <ArrowBackIcon />
+        </Fab>
+      </Box>
+      <Typography variant="h3" gutterBottom> Sales Report Preview</Typography>
       <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
       <Box sx={{ p:6, position: 'fixed', bottom: 0, width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
         <Fab style={{ backgroundColor: 'blue', color: 'white', marginRight: '10px' }} onClick={() => htmlToPdf(htmlContent)}>
