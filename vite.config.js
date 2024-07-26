a9e40ac938f35b9621216c5b0a9204c84fe4503d
Takuya <takuya@asial.co.jp>
2024-07-27 04:07:06 +0900

    Updated project configuration

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+
+const HOST = process.env.MONACA_SERVER_HOST || '0.0.0.0';
+
+export default defineConfig({
+  plugins: [react()],
+  root: './src',
+  base: '',
+  build: {
+    outDir: '../www',
+    minify: false,
+    emptyOutDir: false,
+  },
+  server: {
+    host: HOST,
+    port: 8080,
+  },
+  publicDir: '../public'
+});
